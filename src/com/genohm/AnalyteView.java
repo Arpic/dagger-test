05f02f5cac9d6d96f9a7bc09dfeff3fef92c44ab
maxime <maxime@genohm.com>
2022-01-05 09:13:46 +0100

    Adapt build.gradle for folder structure

--- src/com/genohm/AnalyteView.java
+++ src/com/genohm/AnalyteView.java
@@ -6,12 +6,9 @@ import javax.inject.Singleton;
 @Singleton
 public class AnalyteView {
 
-	private final AnalyteButton analyteButton;
-
 	@Inject
 	public AnalyteView(AnalyteButton analyteButton,
 	                   AnalyteSomething analyteSomething,
 	                   AnalyteButton analyteButton2) {
-		this.analyteButton = analyteButton;
 	}
 }
